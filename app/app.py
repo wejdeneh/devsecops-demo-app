5c075a8498fc68013c81ae716a1033b06c683212
wejdeneh <wejdene.hadjmefteh@esprit.tn>
2026-07-18 13:48:45 +0000

    Add DevSecOps security pipeline

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "DevSecOps Application Running!"
+
+
+@app.route("/health")
+def health():
+    return {"status": "healthy"}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
